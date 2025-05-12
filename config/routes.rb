16c503d8be23573955c603a13c4590536c15575c
Rei Ogasawara <khf_5@icloud.com>
2025-05-12 18:06:32 +0900

    created header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show]
   get 'home/index' => 'home#index'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
