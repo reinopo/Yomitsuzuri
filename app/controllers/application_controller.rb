d174352805e4ca6e2263cc5880dc2c33d3109ad7
Rei Ogasawara <khf_5@icloud.com>
2025-06-17 12:25:22 +0900

    ログイン・ログアウト時にフラッシュ表示しないようメソッドオーバーライド

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || user_path(resource)
+    home_mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 
 Rails.application.routes.draw do
   # 認証
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   # リソース
   resources :users, only: [:show]
