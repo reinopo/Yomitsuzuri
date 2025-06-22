f4362997c83db0a8d5762db92239733d064391aa
Rei Ogasawara <khf_5@icloud.com>
2025-06-22 13:09:45 +0900

    ユーザー情報にニックネームを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 Rails.application.routes.draw do
   # 認証
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
 
   # リソース
