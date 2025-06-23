acef013d81c7c229a6206b363179ecce21aca269
Rei Ogasawara <khf_5@icloud.com>
2025-06-24 07:53:57 +0900

    フラッシュのUIを統一

--- app/javascript/modules/flash.js
+++ app/javascript/modules/flash.js
@@ -1,19 +1,19 @@
 document.addEventListener("turbo:load", () => {
-  // フラッシュ閉じる
-  document.querySelectorAll(".c-flash").forEach((flash) => {
-    const closeBtn = flash.querySelector(".c-flash__close");
-    if (closeBtn) {
-      closeBtn.addEventListener("click", () => flash.remove());
-    }
+  // // フラッシュ閉じる
+  // document.querySelectorAll(".c-flash").forEach((flash) => {
+  //   const closeBtn = flash.querySelector(".c-flash__close");
+  //   if (closeBtn) {
+  //     closeBtn.addEventListener("click", () => flash.remove());
+  //   }
 
-    setTimeout(() => {
-      flash.classList.add("is-fading-out");
-      setTimeout(() => flash.remove(), 500);
-    }, 3000);
-  });
+  //   setTimeout(() => {
+  //     flash.classList.add("is-fading-out");
+  //     setTimeout(() => flash.remove(), 500);
+  //   }, 3000);
+  // });
 
   // プロフィール更新のフラッシュ
-  const profileFlash = document.querySelector(".c-profile-flash");
+  const profileFlash = document.querySelector(".c-flash");
   if (profileFlash) {
     setTimeout(() => {
       profileFlash.classList.add("is-fading-out");
