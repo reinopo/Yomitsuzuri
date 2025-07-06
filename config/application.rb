f4b61fcd4673c9acc461942261efa6e1870fd131
Rei Ogasawara <khf_5@icloud.com>
2025-07-06 13:38:04 +0900

    require cloudinary

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require "cloudinary"
+require "activestorage-cloudinary-service"
+
 module Yomitsuzuri
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
